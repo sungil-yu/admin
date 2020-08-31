eafbe0d1eb59d173b6eefd16cdc3cfd5ba1c9902
chinggin <chinggin@naver.com>
2020-08-31 17:37:43 +0900

    Item API create

---
+++ src/main/java/com/oncerun/admin/controller/api/ItemApiController.java
@@ -0,0 +1,48 @@
+package com.oncerun.admin.controller.api;
+
+import com.oncerun.admin.domain.network.Header;
+import com.oncerun.admin.domain.network.request.ItemApiRequest;
+import com.oncerun.admin.domain.network.response.ItemApiResponse;
+import com.oncerun.admin.itf.CrudInterface;
+import com.oncerun.admin.service.ItemApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
+
+    @Override
+    @PostMapping
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+
+        return itemApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+
+        return null;
+    }
+
+    @Override
+    @PutMapping
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header<ItemApiResponse> delete(@PathVariable Long id) {
+
+        return null;
+    }
+}
---
+++ src/main/java/com/oncerun/admin/service/ItemApiLogicService.java
@@ -0,0 +1,78 @@
+package com.oncerun.admin.service;
+
+import com.oncerun.admin.domain.entity.Item;
+import com.oncerun.admin.domain.network.Header;
+import com.oncerun.admin.domain.network.request.ItemApiRequest;
+import com.oncerun.admin.domain.network.response.ItemApiResponse;
+import com.oncerun.admin.itf.CrudInterface;
+import com.oncerun.admin.repository.ItemRepository;
+import com.oncerun.admin.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> request) {
+
+        ItemApiRequest body = request.getData();
+
+        Item item = Item.builder()
+                .name(body.getName())
+                .brandName(body.getBrandName())
+                .content(body.getContent())
+                .price(body.getPrice())
+                .title(body.getTitle())
+                .status(body.getStatus())
+                .registeredAt(LocalDateTime.now())
+                .partner(partnerRepository.getOne(body.getPartnerId()))
+                .build();
+
+        Item newItem = itemRepository.save(item);
+
+        return response(newItem);
+    }
+
+    @Override
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> update(Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> delete(Long id) {
+        return null;
+    }
+
+
+    private Header<ItemApiResponse> response(Item item){
+
+        ItemApiResponse body = ItemApiResponse
+                .builder()
+                .id(item.getId())
+                .status(item.getStatus())
+                .brandName(item.getBrandName())
+                .name(item.getName())
+                .title(item.getTitle())
+                .price(item.getPrice())
+                .content(item.getContent())
+                .registeredAt(item.getRegisteredAt())
+                .unregisteredAt(item.getUnregisteredAt())
+                .partnerId(item.getPartner().getId())
+                .build();
+
+        return Header.OK(body);
+    }
+
+}
